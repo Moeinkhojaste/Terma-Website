d395f0f2d633b8c96f551b320af3cc856e573136
Moein Khojaste Manesh <moeinkhm82@gmail.com>
2026-09-12 15:07:41 +0330

    feat: add EnamadBadge component for trust seal verification

--- frontend/src/components/ui/enamad.tsx
+++ frontend/src/components/ui/enamad.tsx
@@ -6,7 +6,7 @@ export type EnamadBadgeProps = {
   className?: string;
 };
 
-const rawEnamadHtml = `<a referrerpolicy='origin' target='_blank' href='https://trustseal.enamad.ir/?id=7710569&Code=xDHmhMgAjw7KUdF1p22YTRX47sE5EHN7'><img referrerpolicy='origin' src='https://trustseal.enamad.ir/logo.aspx?id=7710569&Code=xDHmhMgAjw7KUdF1p22YTRX47sE5EHN7' alt='' style='cursor:pointer' code='xDHmhMgAjw7KUdF1p22YTRX47sE5EHN7'></a>`;
+const rawEnamadHtml = `<a referrerpolicy='origin' target='_blank' href='https://trustseal.enamad.ir/?id=7710569&code=xDHmhMgAjw7KUdF1p22YTRX47sE5EHN7'><img referrerpolicy='origin' src='https://trustseal.enamad.ir/logo.aspx?id=7710569&Code=xDHmhMgAjw7KUdF1p22YTRX47sE5EHN7' alt='' style='cursor:pointer' code='xDHmhMgAjw7KUdF1p22YTRX47sE5EHN7'></a>`;
 
 export function EnamadBadge({ className = "" }: EnamadBadgeProps) {
   return (
